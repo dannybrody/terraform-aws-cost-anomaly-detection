d2bc1fc815d3c4df53c0bf3b64bd8982ef80dd3f
manu-caylent <manuel.palacios@caylent.com>
2023-04-03 10:53:57 -0300

    update module source in examples

--- examples/example.tf
+++ examples/example.tf
@@ -1,6 +1,6 @@
 module "cost_anomaly_detector" {
-  source             = "git@github.com:jmpcba/cost_anomaly_detection.gitref=master"
-  cost_threshold     = 0.1
+  source             = "git@github.com:manu-caylent/cost_anomaly_monitor.git?ref=main"
+  cost_threshold     = 10
   threshold_type     = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
   slack_channel_id   = var.slack_channel_id
   slack_workspace_id = var.slack_workspace_id
