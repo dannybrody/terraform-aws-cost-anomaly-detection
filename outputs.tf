b7c1e8b2b6413e0cdc2d2d6783ac8538f32876e3
manu-caylent <manuel.palacios@caylent.com>
2023-04-05 15:05:20 -0300

    adding output file

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "sns_topic_arn" {
+  value = aws_sns_topic.cost_anomaly_topic.arn
+}
+
+output "anomaly_monitor_arn" {
+  value = aws_ce_anomaly_monitor.anomaly_monitor.arn
+}
+
+output "anomaly_subscription_arn" {
+  value = aws_ce_anomaly_subscription.anomaly_subscription.arn
+}
