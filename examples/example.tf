fe8b24e55890bc1ce58f48a0b174cbc4aa4ce333
Manu <manuel.palacios@caylent.com>
2023-04-05 21:57:42 -0300

    remove tfvars

--- examples/example.tf
+++ examples/example.tf
@@ -1,7 +1,7 @@
 module "cost_anomaly_detector" {
   source             = "git@github.com:manu-caylent/cost_anomaly_monitor.git?ref=v1.0.0"
-  cost_threshold     = 10
-  threshold_type     = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
+  cost_threshold     = 0.1
+  threshold_type     = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
   slack_channel_id   = var.slack_channel_id
   slack_workspace_id = var.slack_workspace_id
 
--- examples/variables.tfvars
+++
@@ -1,3 +0,0 @@
-slack_channel_id = "C050M0WKT4H"
-slack_workspace_id = "TPR2J0BM3"
-region = "us-east-1"
