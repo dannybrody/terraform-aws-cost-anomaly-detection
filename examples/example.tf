a274f80dcd1b9e24bae25a2f839fe29aebbe03ce
Manu <manuel.palacios@caylent.com>
2023-04-08 23:10:56 -0300

    bump version to v1.0.1

--- examples/example.tf
+++ examples/example.tf
@@ -1,5 +1,5 @@
 module "cost_anomaly_detector" {
-  source             = "git@github.com:manu-caylent/cost_anomaly_monitor.git?ref=v1.0.0"
+  source             = "git@github.com:manu-caylent/cost_anomaly_monitor.git?ref=v1.0.1"
   cost_threshold     = 0.1
   threshold_type     = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
   slack_channel_id   = var.slack_channel_id
