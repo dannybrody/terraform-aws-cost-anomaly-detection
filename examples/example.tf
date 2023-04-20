5e15071e3eda26d85bec5ae17b0bfc65ea1c2783
manu-caylent <manuel.palacios@caylent.com>
2023-04-20 11:17:44 -0300

    point example to latest tag

--- examples/example.tf
+++ examples/example.tf
@@ -1,7 +1,7 @@
 # Module configuration to setup an absolute impact monitor for several accounts
 # Must be deployed on the root account to work
 module "multi_account_cost_anomaly_detector" {
-  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.0.0"
+  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.1.0"
   alert_threshold    = 0.1
   threshold_type     = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
   slack_channel_id   = var.slack_channel_id
@@ -20,7 +20,7 @@ module "multi_account_cost_anomaly_detector" {
 # For example purposes only. This will only work if terraform apply is run with -target=module.multi_account_cost_anomaly_detector
 # This module uses a percentage threshold, monitors services in just one account, and uses the sns topic of the multi account module above.
 module "service_cost_anomaly_detector" {
-  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.0.0"
+  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.1.0"
   alert_threshold    = 20
   threshold_type     = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
   slack_channel_id   = var.slack_channel_id
