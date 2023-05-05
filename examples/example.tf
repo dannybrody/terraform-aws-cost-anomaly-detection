f48c5921ab2d019c990fd203515f184e38d3465a
manu-caylent <manuel.palacios@caylent.com>
2023-05-05 13:49:42 -0300

    update examples

--- examples/example.tf
+++ examples/example.tf
@@ -1,14 +1,11 @@
-# Module configuration to setup an absolute impact monitor for several accounts
-# Must be deployed on the root account to work
-module "multi_account_cost_anomaly_detector" {
-  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.1.0"
-  alert_threshold    = 0.1
+# Module configuration to setup an absolute impact monitor for one account
+# This is the config recommended by AWS
+module "service_cost_anomaly_detector" {
+  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.2.0"
+  alert_threshold    = 20 # $20, because threshold_type is ANOMALY_TOTAL_IMPACT_ABSOLUTE
   threshold_type     = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
   slack_channel_id   = var.slack_channel_id
   slack_workspace_id = var.slack_workspace_id
-  accounts = [
-    # list of AWS accounts to monitor
-  ]
 
   tags = {
     key                = "value"
@@ -17,16 +14,18 @@ module "multi_account_cost_anomaly_detector" {
   }
 }
 
-# For example purposes only. This will only work if terraform apply is run with -target=module.multi_account_cost_anomaly_detector
-# This module uses a percentage threshold, monitors services in just one account, and uses the sns topic of the multi account module above.
-module "service_cost_anomaly_detector" {
-  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.1.0"
-  alert_threshold    = 20
+
+# Module configuration to setup an absolute impact monitor for several accounts
+# Must be deployed on the root account to work
+module "multi_account_cost_anomaly_detector" {
+  source             = "github.com/caylent/terraform-aws-cost-anomaly-detection.git?ref=v1.2.0"
+  alert_threshold    = 10 # 10%, because threshold_type is ANOMALY_TOTAL_IMPACT_PERCENTAGE
   threshold_type     = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
   slack_channel_id   = var.slack_channel_id
   slack_workspace_id = var.slack_workspace_id
-  multi_account = false
-  sns_topic_arn = module.multi_account_cost_anomaly_detector.sns_topic_arn
+  accounts = [
+    # list of AWS accounts to monitor
+  ]
 
   tags = {
     key                = "value"
