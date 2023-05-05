32ad13924159bcf25c3bd7942c72ba2a4decefa2
manu-caylent <manuel.palacios@caylent.com>
2023-05-05 11:46:21 -0300

    typo in length function

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   slack_integration = var.sns_topic_arn != "" || var.enable_slack_integration == false ? 0 : 1
-  multi_account = len(var.accounts) == 0 ? false : true
+  multi_account = length(var.accounts) == 0 ? false : true
 }
